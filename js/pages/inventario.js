3f2ccd84af52e39fbc40aed2950bf1b64889228a
maria <mvbri10@gmail.com>
2021-03-02 14:02:03 -0400

    Avance

--- js/pages/inventario.js
+++ js/pages/inventario.js
@@ -1,27 +1,2 @@
 
 
-$(document).ready(function(){
-
-    $("#menu").toggle(function(){
-    
-      $("#nav").stop().animate(
-      
-       {
-         left:"0px"
-         
-       },500, "easeInSine");
-  
-    },
-    
-    function(){
-     
-     $("#nav").stop().animate(
-     
-       {
-         left:"-310px"
-       },500);
-       
-   });
-     
-  
-  });
--- js/pages/login.js
+++ js/pages/login.js
@@ -64,3 +64,49 @@ function registro(){
 }
 
 
+const formulario = document.getElementById('formulario_registro');
+const inputs = document.querySelectorAll('#formulario_registro input');
+
+const expresiones = {
+	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	password: /^.{4,12}$/, // 4 a 12 digitos.
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+}
+
+
+const validar_formulario = (e) => {
+    switch(e.target.name) {
+       case 'nombre_u':
+           if(expresiones.usuario.test(e.target.value)) {
+            document.getElementById('grupo_usuario').classList.remove('formulario_grupo_incorrecto');
+            document.getElementById('grupo_usuario').classList.add('formulario_correcto');
+            document.querySelector('#grupo_usuario i').classList.remove('fa-times-circle');
+            document.querySelector('#grupo_usuario i').classList.add('fa-check-circle');
+           } else {
+              document.getElementById('grupo_usuario').classList.add('formulario_grupo_incorrecto');
+           }
+       break;
+       case 'correo':
+
+       break;
+       case 'password':
+
+       break;
+       case 'nombre_c':
+
+       break;
+    }
+}
+
+
+inputs.forEach( (input) =>{
+   input.addEventListener('keyup', validar_formulario);
+   input.addEventListener('blur', validar_formulario);
+}); 
+
+
+formulario.addEventListener('submit', (e) => {
+   e.preventDefault();
+})
