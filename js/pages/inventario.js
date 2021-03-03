eac7f7186f02c586fde9a7b6b79c9f0164dc5b6f
maria <mvbri10@gmail.com>
2021-03-02 22:17:08 -0400

    Avance

--- js/pages/inventario.js
+++ js/pages/inventario.js
@@ -1,2 +1,10 @@
 
 
+const passwordChange = () => {
+    const inputPassword1 = document.getElementById('password_change');
+    const inputPassword1 = document.getElementById('password_change_confirm');
+
+    if(inputPassword1 !== inputPassword1.value) {
+        alert("las contraseñas no son iguales");
+    }
+}
